429af7ccc04e4d2d885bde6ba31ebd7e4fe2366c
Jason A. Wise <jasonandrewwise@gmail.com>
2021-11-14 17:34:05 -0500

    adding storybook

---
+++ src/components/ConditionsDisplay/ConditionsDisplay.js
@@ -0,0 +1,5 @@
+function ConditionsDisplay() {
+	return <h2>Conditions</h2>
+}
+
+export default ConditionsDisplay
---
+++ src/components/LocationSearch/LocationSearch.js
@@ -0,0 +1,11 @@
+function LocationSearch() {
+	return (
+		<>
+			<h2>Location Search</h2>
+			<label htmlFor="location">Enter, city or zip to get conditions</label>
+			<input type="text" name="location" id="location" />
+		</>
+	)
+}
+
+export default LocationSearch
